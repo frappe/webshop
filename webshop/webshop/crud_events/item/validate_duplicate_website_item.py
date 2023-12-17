fe420333f68fa4b037fd8d6a72a73f546d6dff3c
Ankush Menat <ankush@frappe.io>
2023-12-17 11:04:10 +0530

    fix: positional argument order for before_rename

--- webshop/webshop/crud_events/item/validate_duplicate_website_item.py
+++ webshop/webshop/crud_events/item/validate_duplicate_website_item.py
@@ -8,7 +8,7 @@ class DataValidationError(frappe.ValidationError):
     pass
 
 
-def execute(doc, old_name, new_name, merge=False):
+def execute(doc, method=None, old_name=None, new_name=None, merge=False):
     """
     Block merge if both old and new items have website items against them.
     This is to avoid duplicate website items after merging.
