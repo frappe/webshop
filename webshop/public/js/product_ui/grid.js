7cf21cebf21ae5e4a2f1360a99cb65667d20f895
rohankumbhar-01 <rkrohankumbhar@gmail.com>
2025-05-05 12:23:33 +0530

    fix: fixed get_primary_button

--- webshop/public/js/product_ui/grid.js
+++ webshop/public/js/product_ui/grid.js
@@ -170,7 +170,7 @@ webshop.ProductGrid = class {
 					</div>
 				</a>
 			`;
-		} else if (settings.enabled && (settings.allow_items_not_in_stock || item.in_cart)) {
+		} else if (settings.enabled && (settings.allow_items_not_in_stock || item.in_stock)) {
 			return `
 				<div style="display: flex; align-items: center;">
 					<!-- Add to Cart Button -->
