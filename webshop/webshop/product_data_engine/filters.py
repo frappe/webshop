60ea72506f57c1b790c994be0bace51f44afb231
rohitwaghchaure <rohitw1991@gmail.com>
2025-05-10 17:29:30 +0530

    fix: filter order (#299)
    (cherry picked from commit 67ff4eeb93931f6ef3711278017b2d8147322f9c)

--- webshop/webshop/product_data_engine/filters.py
+++ webshop/webshop/product_data_engine/filters.py
@@ -131,8 +131,13 @@ class ProductFiltersBuilder:
 			attribute_value_map.setdefault(d.attribute, []).append(d.attribute_value)
 
 		out = []
-		for name, values in attribute_value_map.items():
-			out.append(frappe._dict(name=name, item_attribute_values=values))
+		for attribute in attributes:
+			if attribute not in attribute_value_map:
+				continue
+
+			values = attribute_value_map[attribute]
+			out.append(frappe._dict(name=attribute, item_attribute_values=values))
+
 		return out
 
 	def get_discount_filters(self, discounts):
