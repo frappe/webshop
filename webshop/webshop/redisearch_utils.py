ca65be103c86c26b3bb9c8c2e39a2806e356878d
alaalsalam <alaalsalam101@gmail.com>
2025-05-18 17:49:38 +0200

    fix delete website item error

--- webshop/webshop/redisearch_utils.py
+++ webshop/webshop/redisearch_utils.py
@@ -158,7 +158,9 @@ def delete_item_from_index(website_item_doc):
 def delete_from_ac_dict(website_item_doc):
 	"""Removes this items's name from autocomplete dictionary"""
 	ac = frappe.cache().ft()
-	ac.sugdel(website_item_doc.web_item_name)
+	# ac.sugdel(website_item_doc.web_item_name)
+	ac.sugdel("autocomplete_key", website_item_doc.web_item_name)
+
 
 
 @if_redisearch_enabled
