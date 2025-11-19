69cfc4fcffcfb57e4ce4339fd65dbaabed4efdb7
stravo1@mac <stravoone@gmail.com>
2025-11-19 09:53:39 +0530

    fix: cleanup after uninstall

--- webshop/setup/install.py
+++ webshop/setup/install.py
@@ -4,7 +4,7 @@ import frappe
 from frappe import _
 from frappe.custom.doctype.custom_field.custom_field import create_custom_fields
 
-from webshop.webshop.utils.setup import has_ecommerce_fields
+from webshop.webshop.utils.setup import has_ecommerce_fields, get_custom_fields
 
 def after_install():
 	run_patches()
@@ -63,121 +63,7 @@ def remove_ecommerce_settings_doctype():
 
 
 def add_custom_fields():
-	custom_fields = {
-		"Item": [
-			{
-				"default": 0,
-				"depends_on": "published_in_website",
-				"fieldname": "published_in_website",
-				"fieldtype": "Check",
-				"ignore_user_permissions": 1,
-				"insert_after": "default_manufacturer_part_no",
-				"label": "Published In Website",
-				"read_only": 1,
-				"no_copy": 1,
-			}
-		],
-		"Item Group": [
-			{
-				"fieldname": "custom_website_settings",
-				"fieldtype": "Section Break",
-				"label": "Website Settings",
-				"insert_after": "taxes",
-			},
-			{
-				"default": "0",
-				"description": "Make Item Group visible in website",
-				"fieldname": "show_in_website",
-				"fieldtype": "Check",
-				"label": "Show in Website",
-				"insert_after": "custom_website_settings",
-			},
-			{
-				"depends_on": "show_in_website",
-				"fieldname": "route",
-				"fieldtype": "Data",
-				"label": "Route",
-				"no_copy": 1,
-				"unique": 1,
-				"insert_after": "show_in_website",
-			},
-			{
-				"depends_on": "show_in_website",
-				"fieldname": "website_title",
-				"fieldtype": "Data",
-				"label": "Title",
-				"insert_after": "route",
-			},
-			{
-				"depends_on": "show_in_website",
-				"description": "HTML / Banner that will show on the top of product list.",
-				"fieldname": "description",
-				"fieldtype": "Text Editor",
-				"label": "Description",
-				"insert_after": "website_title",
-			},
-			{
-				"default": "0",
-				"depends_on": "show_in_website",
-				"description": "Include Website Items belonging to child Item Groups",
-				"fieldname": "include_descendants",
-				"fieldtype": "Check",
-				"label": "Include Descendants",
-				"insert_after": "website_title",
-			},
-			{
-				"fieldname": "column_break_16",
-				"fieldtype": "Column Break",
-				"insert_after": "include_descendants",
-			},
-			{
-				"depends_on": "show_in_website",
-				"fieldname": "weightage",
-				"fieldtype": "Int",
-				"label": "Weightage",
-				"insert_after": "column_break_16",
-			},
-			{
-				"depends_on": "show_in_website",
-				"description": "Show this slideshow at the top of the page",
-				"fieldname": "slideshow",
-				"fieldtype": "Link",
-				"label": "Slideshow",
-				"options": "Website Slideshow",
-				"insert_after": "weightage",
-			},
-			{
-				"depends_on": "show_in_website",
-				"fieldname": "website_specifications",
-				"fieldtype": "Table",
-				"label": "Website Specifications",
-				"options": "Item Website Specification",
-				"insert_after": "description",
-			},
-			{
-				"collapsible": 1,
-				"depends_on": "show_in_website",
-				"fieldname": "website_filters_section",
-				"fieldtype": "Section Break",
-				"label": "Website Filters",
-				"insert_after": "website_specifications",
-			},
-			{
-				"fieldname": "filter_fields",
-				"fieldtype": "Table",
-				"label": "Item Fields",
-				"options": "Website Filter Field",
-				"insert_after": "website_filters_section",
-			},
-			{
-				"fieldname": "filter_attributes",
-				"fieldtype": "Table",
-				"label": "Attributes",
-				"options": "Website Attribute",
-				"insert_after": "filter_fields",
-			},
-		]
-	}
+	custom_fields = get_custom_fields()
 
 	frappe.make_property_setter(
 		{
--- webshop/webshop/utils/setup.py
+++ webshop/webshop/utils/setup.py
@@ -11,3 +11,120 @@ def has_ecommerce_fields() -> bool:
 
 	data = query.run(as_dict=True)
 	return bool(data)
+
+def get_custom_fields():
+    return {
+		"Item": [
+			{
+				"default": 0,
+				"depends_on": "published_in_website",
+				"fieldname": "published_in_website",
+				"fieldtype": "Check",
+				"ignore_user_permissions": 1,
+				"insert_after": "default_manufacturer_part_no",
+				"label": "Published In Website",
+				"read_only": 1,
+				"no_copy": 1,
+			}
+		],
+		"Item Group": [
+			{
+				"fieldname": "custom_website_settings",
+				"fieldtype": "Section Break",
+				"label": "Website Settings",
+				"insert_after": "taxes",
+			},
+			{
+				"default": "0",
+				"description": "Make Item Group visible in website",
+				"fieldname": "show_in_website",
+				"fieldtype": "Check",
+				"label": "Show in Website",
+				"insert_after": "custom_website_settings",
+			},
+			{
+				"depends_on": "show_in_website",
+				"fieldname": "route",
+				"fieldtype": "Data",
+				"label": "Route",
+				"no_copy": 1,
+				"unique": 1,
+				"insert_after": "show_in_website",
+			},
+			{
+				"depends_on": "show_in_website",
+				"fieldname": "website_title",
+				"fieldtype": "Data",
+				"label": "Title",
+				"insert_after": "route",
+			},
+			{
+				"depends_on": "show_in_website",
+				"description": "HTML / Banner that will show on the top of product list.",
+				"fieldname": "description",
+				"fieldtype": "Text Editor",
+				"label": "Description",
+				"insert_after": "website_title",
+			},
+			{
+				"default": "0",
+				"depends_on": "show_in_website",
+				"description": "Include Website Items belonging to child Item Groups",
+				"fieldname": "include_descendants",
+				"fieldtype": "Check",
+				"label": "Include Descendants",
+				"insert_after": "website_title",
+			},
+			{
+				"fieldname": "column_break_16",
+				"fieldtype": "Column Break",
+				"insert_after": "include_descendants",
+			},
+			{
+				"depends_on": "show_in_website",
+				"fieldname": "weightage",
+				"fieldtype": "Int",
+				"label": "Weightage",
+				"insert_after": "column_break_16",
+			},
+			{
+				"depends_on": "show_in_website",
+				"description": "Show this slideshow at the top of the page",
+				"fieldname": "slideshow",
+				"fieldtype": "Link",
+				"label": "Slideshow",
+				"options": "Website Slideshow",
+				"insert_after": "weightage",
+			},
+			{
+				"depends_on": "show_in_website",
+				"fieldname": "website_specifications",
+				"fieldtype": "Table",
+				"label": "Website Specifications",
+				"options": "Item Website Specification",
+				"insert_after": "description",
+			},
+			{
+				"collapsible": 1,
+				"depends_on": "show_in_website",
+				"fieldname": "website_filters_section",
+				"fieldtype": "Section Break",
+				"label": "Website Filters",
+				"insert_after": "website_specifications",
+			},
+			{
+				"fieldname": "filter_fields",
+				"fieldtype": "Table",
+				"label": "Item Fields",
+				"options": "Website Filter Field",
+				"insert_after": "website_filters_section",
+			},
+			{
+				"fieldname": "filter_attributes",
+				"fieldtype": "Table",
+				"label": "Attributes",
+				"options": "Website Attribute",
+				"insert_after": "filter_fields",
+			},
+		]
+	}
