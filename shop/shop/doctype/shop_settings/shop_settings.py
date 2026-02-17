8e7d758e5fac77ba2ddd60344c2f1a0dae116b9c
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-02-17 11:12:22 +0530

    fix(revert): remove invalid ignore_permissions arg from get_cached_doc

--- shop/shop/doctype/shop_settings/shop_settings.py
+++ shop/shop/doctype/shop_settings/shop_settings.py
@@ -164,7 +164,7 @@ def validate_cart_settings(doc=None, method=None):
 
 
 def get_shopping_cart_settings():
-	return frappe.get_cached_doc("Shop Settings", ignore_permissions=True)
+	return frappe.get_cached_doc("Shop Settings")
 
 
 @frappe.whitelist(allow_guest=True)
