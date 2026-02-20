2078f54e0166c641030eeb4faf2b577681b1f082
Balaji K <balajik@Balajis-MacBook-Air.local>
2026-02-20 10:53:15 +0530

    fix: reload quotation doc after auto-setting addresses to ensure UI reflects latest state

--- shop/shop/shopping_cart/cart.py
+++ shop/shop/shopping_cart/cart.py
@@ -44,9 +44,11 @@ def get_cart_quotation(doc=None):
 
 	if not doc.customer_address and addresses:
 		update_cart_address("billing", addresses[0].name)
+		doc = _get_cart_quotation(party)
 
 	if not doc.shipping_address_name and get_shipping_addresses(party):
 		update_cart_address("shipping", get_shipping_addresses(party)[0]["name"])
+		doc = _get_cart_quotation(party)
 
 	return {
 		"doc": decorate_quotation_doc(doc),
