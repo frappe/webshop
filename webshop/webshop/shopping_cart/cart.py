ff6a8f0c120dace7aacb2260180a88750a526947
stravo1 <stravoone@gmail.com>
2026-06-05 13:50:32 +0530

    fix: fallback to old import for older ERPNext versions

--- webshop/webshop/shopping_cart/cart.py
+++ webshop/webshop/shopping_cart/cart.py
@@ -14,7 +14,11 @@ from webshop.webshop.doctype.webshop_settings.webshop_settings import (
     get_shopping_cart_settings,
 )
 from webshop.webshop.utils.product import get_web_item_qty_in_stock
-from erpnext.selling.doctype.quotation.mapper import _make_sales_order
+
+try:
+	from erpnext.selling.doctype.quotation.mapper import _make_sales_order
+except ImportError: # for older ERPNext versions (versioning issue)
+	from erpnext.selling.doctype.quotation.quotation import _make_sales_order
 
 
 class WebsitePriceListMissingError(frappe.ValidationError):
