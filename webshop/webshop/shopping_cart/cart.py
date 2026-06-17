fc541fefde420796c80911634b1094ffa8dbc23c
stravo1 <stravoone@gmail.com>
2026-06-17 15:33:21 +0530

    fix: bad import from erpnext

--- webshop/webshop/shopping_cart/cart.py
+++ webshop/webshop/shopping_cart/cart.py
@@ -14,7 +14,10 @@ from webshop.webshop.doctype.webshop_settings.webshop_settings import (
     get_shopping_cart_settings,
 )
 from webshop.webshop.utils.product import get_web_item_qty_in_stock
-from erpnext.selling.doctype.quotation.quotation import _make_sales_order
+try:
+	from erpnext.selling.doctype.quotation.quotation import _make_sales_order
+except ImportError:
+	from erpnext.selling.doctype.quotation.mapper import _make_sales_order
 
 
 class WebsitePriceListMissingError(frappe.ValidationError):
