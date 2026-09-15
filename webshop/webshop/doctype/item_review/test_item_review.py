8f055754c5548ed5409c0b16d00ff78dc661fcca
barredterra <14891507+barredterra@users.noreply.github.com>
2026-09-15 02:49:28 +0200

    test: rating value from 0..1

--- webshop/webshop/doctype/item_review/test_item_review.py
+++ webshop/webshop/doctype/item_review/test_item_review.py
@@ -51,7 +51,7 @@ class TestItemReview(unittest.TestCase):
 
 		# post review on "Test Mobile Phone"
 		try:
-			add_item_review(web_item, "Great Product", 4, "Would recommend this product")
+			add_item_review(web_item, "Great Product", 4 / 5, "Would recommend this product")
 			review_name = frappe.db.get_value("Item Review", {"website_item": web_item})
 		except Exception:
 			self.fail(f"Error while publishing review for {web_item}")
@@ -60,7 +60,7 @@ class TestItemReview(unittest.TestCase):
 
 		self.assertEqual(len(review_data.reviews), 1)
 		self.assertTrue(review_data.average_rating)
-		self.assertEqual(review_data.reviews_per_rating[0], 100)
+		self.assertEqual(review_data.reviews_per_rating[3], 100)  # 4 out of 5 stars
 
 		# tear down
 		frappe.set_user("Administrator")
